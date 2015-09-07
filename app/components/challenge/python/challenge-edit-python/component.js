db31801b8afe97d8d763d54ada0edbf54c60e8c7
Amr Draz <amr.m.draz@gmail.com>
2015-09-08 01:42:34 +0200

    python debugger now fully functional plus editor displays errors running python code is complete
    next step is testing
    
    improvment can be made to show runtime errors while debugging on step rather than from the start of the code run
    since I think it would discourage students from playing the trace

--- app/components/challenge/python/challenge-edit-python/component.js
+++ app/components/challenge/python/challenge-edit-python/component.js
@@ -11,58 +11,112 @@ var $io = {
 $io.__mro__ = [$io, _b_.object.$dict];
 
 export default Ember.Component.extend({
+    canStep: false,
+    isDebugging: false,
+    isLastStep: false,
+    isFirstStep: false,
     evaluatedModelProperty: 'solution',
     writeToConsole(data, type) {
-        this.EventBus.publish('console.write', data + '\n', type);
+        this.EventBus.publish('console.write', data, type);
     },
     clearConsole() {
         this.EventBus.publish('console.clear');
     },
+    clearLint(){
+        this.EventBus.publish('editor.lint', this.get('evaluatedModelProperty'), []);
+    },
+    goToLine(line){
+        this.EventBus.publish('editor.line', this.get('evaluatedModelProperty'), line);
+    },
     resetSrc() {
         var component = this;
         component.get('models').set(component.get('evaluatedModelProperty'), "");
-        component.EventBus.publish('editor.line', component.get('evaluatedModelProperty'), 0);
+        component.goToLine(0);
     },
     runCode(src) {
         this.clearConsole();
+        this.clearLint();
         this.EventBus.publish('console.show');
-
         var t0 = Date.now();
-        var module_name = '__main__';
-        $B.$py_module_path[module_name] = window.location.href;
-        try {
-            var root = $B.py2js(src, module_name, module_name, '__builtins__');
-            //earney
-            var js = root.to_js();
-            if ($B.debug > 1) {
-                console.log(js);
-            }
+        Debugger.run_no_debugger(src);
+        this.writeToConsole('\n<completed in ' + ((Date.now() - t0) * 1000.0) + ' ms >\n');
 
-            var None = _b_.None;
-            var getattr = _b_.getattr;
-            var setattr = _b_.setattr;
-
-            if ($B.async_enabled) {
-                js = $B.execution_object.source_conversion(js);
-
-                //console.log(js)
-                eval(js);
-            } else {
-                // Run resulting Javascript
-                eval(js);
-            }
-        } catch (exc) {
-            $B.leave_frame();
-            $B.leave_frame();
-            if (exc.$py_error) {
-                this.writeToConsole(_b_.getattr(exc, 'info') + '\n' + _b_.getattr(exc, '__name__') + ": " + exc.$message + '\n');
-            } else {
-                throw exc;
-            }
+    },
+    startDebugger(src) {
+        this.clearConsole();
+        this.clearLint();
+        this.EventBus.publish('console.show');
+        Debugger.start_debugger(src, true);
+    },
+    stopDebugger() {
+        Debugger.stop_debugger();
+    },
+    stepDebugger() {
+        if (!Debugger.is_debugging()) {
+            this.startDebugger();
+        } else {
+            Debugger.step_debugger();
         }
+    },
+    stepBackDebugger() {
+        Debugger.step_back_debugger();
+    },
+    debug_started() {
+        this.set('isLastStep', false);
+        this.set('isFirstStep', true);
+        this.set('isDebugging', Debugger.is_debugging());
+        // doc('run').disabled = true;
+        // doc('debug').disabled = true;
+        // doc('step').disabled = false;
+        // doc('stop').disabled = false;
+        if (Debugger.is_recorded()) {
+            if (Debugger.get_recorded_states().length > 0) {
+                this.goToLine(Debugger.get_recorded_states()[0].next_line_no);
+            } else {
+                Debugger.stop_debugger();
+            }
+        } else {
+            Debugger.step_debugger();
+        }
+    },
+    debug_stoped() {
+        this.set('isDebugging', Debugger.is_debugging());
+        // doc('debug').disabled = false;
+        // doc('run').disabled = false;
+        // doc('step').disabled = true;
+        // doc('back').disabled = true;
+        // doc('stop').disabled = true;
+    },
+    debug_step(state) {
+        this.clearConsole();
+        this.writeToConsole(state.stdout + '\n');
 
-        this.writeToConsole('<completed in ' + ((Date.now() - t0) * 1000.0) + ' ms >');
+        this.goToLine(state.next_line_no);
 
+        this.set('isLastStep', Debugger.is_last_step());
+        this.set('canStep', Debugger.can_step());
+        this.set('isFirstStep', Debugger.is_first_step());
+    },
+    debug_error(err, Debugger) {
+        // This is a syntax errorr
+        if (Debugger.get_recorded_states().length === 0) {
+            // doc('console').value = err.data;
+            this.writeToConsole(err.data + '\n');
+            Debugger.stop_debugger();
+        }
+        // {
+        //         line_no: (line) - 1,
+        //         column_no_start: column_no_start || 0,
+        //         column_no_stop: column_no_stop || 200,
+        //         message: msg,
+        //         fragment: fragment || '',
+        //         severity: "error"
+        //     }
+        err.column_no_start = 0;
+        err.column_no_stop = 200;
+        err.severity = 'error';
+
+        this.EventBus.publish('editor.lint', this.get('evaluatedModelProperty'), [err]);
     },
     actions: {
         run() {
@@ -72,16 +126,16 @@ export default Ember.Component.extend({
             this.sendAction(this.get('run'));
         },
         step() {
-            this.sendAction(this.get('run'));
+            this.stepDebugger();
         },
         back() {
-            this.sendAction(this.get('run'));
+            this.stepBackDebugger();
         },
         debug() {
-            this.sendAction(this.get('run'));
+            this.startDebugger(this.get('model').get(this.get('evaluatedModelProperty')));
         },
         stop() {
-            this.sendAction(this.get('run'));
+            this.stopDebugger();
         },
         reset() {
             this.sendAction(this.get('run'));
@@ -101,6 +155,13 @@ export default Ember.Component.extend({
         };
         $B.stdout = cout;
         $B.stderr = cout;
+
+        this.set('Debugger', Debugger);
+
+        Debugger.on_debugging_started(component.debug_started.bind(component));
+        Debugger.on_debugging_end(component.debug_stoped.bind(component));
+        Debugger.on_debugging_error(component.debug_error.bind(component));
+        Debugger.on_step_update(component.debug_step.bind(component));
 
     },
     willClearRender() {
