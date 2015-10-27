3f2139dcc142eda0504b5ea63e5fc3a506fdd1c6
Amr Draz <amr.m.draz@gmail.com>
2015-10-27 05:58:01 +0200

    updated test documentation made get report return the test repot with score unlimited by max score updated how testError handles error based on recent bugs

--- vendor/debugger/tester.js
+++ vendor/debugger/tester.js
@@ -3,6 +3,12 @@
  * It adds a global Brython_Tester containing all the API functions
  */
 (function(win) {
+
+    var $B = window.__BRYTHON__;
+    var _b_ = $B.builtins;
+    var Debugger = window.Brython_Debugger;
+    var _ = window._;
+
     var Test = win.Brython_Tester = {
         init: init,
         get_code: getCode,
@@ -15,6 +21,7 @@
         add_test: appendTestToReport,
         ok: pass,
         fail: fail,
+        assert: assert,
         expect: expect,
         expect_for: expectFor,
         matches: matches,
@@ -22,11 +29,6 @@
         exists: exists,
         on_test_error: callbackSetter('testError'),
     };
-
-    var $B = window.__BRYTHON__;
-    var _b_ = $B.builtins;
-    var Debugger = window.Brython_Debugger;
-    var _ = window._;
 
     var noop = function() {};
     var events = ['testError'];
@@ -55,6 +57,10 @@
         tests: []
     };
 
+    /**
+     * Initialise the Test object's and brython (should it not have ran before)
+     * imports the io module inorder to use StringIO to mock input stream
+     */
     function init() {
         restTest();
         scope = firstRunCheck();
@@ -62,6 +68,12 @@
         scope.StringIO = _b_.getattr($B.imported.io, "StringIO");
     }
 
+    /**
+     * define Module in brython prgramatically
+     * used to inject Brython_Tester as a module called Test
+     * @param  {[type]} name [description]
+     * @param  {[type]} mod  [description]
+     */
     function defineModule(name, mod) {
         mod.__class__ = $B.$ModuleDict;
         mod.__name__ = name;
@@ -71,18 +83,52 @@
         $B.imported[name] = $B.modules[name] = mod;
     }
 
+    /**
+     * returns teh current test's code
+     * @return {String} code test is runniing against
+     */
     function getCode() {
         return code;
     }
 
+    /**
+     * sets code to run test against
+     */
     function setCode(c) {
         code = c;
     }
 
+    /**
+     * returns the test report object
+     * the report is of the form
+     *     {Boolean} passed wether the entier test suite passed or not
+     *     {Number} score the acumilated score the user quired from the test
+     *     {[Test]} passes an array of the tests that passed
+     *     {[Test]} failures an array of the tests that failed
+     *     {[Test]} tests an array of all tests
+     *     A single test object is of the form
+     *         {Booelan} passed wether this test passed or not
+     *         {String}  message the message that is to be displayed
+     *         {Number}  score   the score awarded for passing this test
+     *         {String}  tag     a string labeling this test in case passing it is speccial
+     * @return {Object} [description]
+     */
     function getReport() {
+
+        var tests = report.tests;
+        report.passes = _.filter(tests, 'passed');
+        report.failures = _.reject(tests, 'passed');
+        report.score = _.reduce(tests, function(sum, t) {
+            return sum + t.score;
+        }, 0);
+        report.passed = report.passes.length === tests.length && report.passes.length>0;
+
         return report;
     }
 
+    /**
+     * restes test back to initial state with empty report and no code set
+     */
     function restTest() {
         report = report = {
             passed: false,
@@ -122,14 +168,14 @@
     }
 
     /**
-     * unsets the input stream back to normal
+     * unsets the input stream back to the original strin
      */
     function unsetInput() {
         did_set_input = false;
         $B.stdin = $B.modules._sys.stdin = scope.__stdin__;
     }
     /**
-     * unsets the input stream back to normal
+     * unsets the input stream back to original stdout and stderr
      */
     function unsetOutput() {
         did_set_output = false;
@@ -137,6 +183,19 @@
         $B.stderr = $B.modules._sys.stderr = scope.__stderr__;
     }
 
+    /**
+     * run code that is to be tested
+     * @return {Object} history the debugger session of run, an object used to get information about the run
+     *                          {String}  stdout  The final output to the console
+     *                          {[State]} prints  An array that is composed of every time the print function
+     *                                            Has a property String data containing the actal line printed
+     *                                            Has a property String stdout containing the overal prints sofar
+     *                          {Object}  locals  A dictionarry containing all the variables declared in the scope of the last frame that ran
+     *                          {Object}  globals A dictionarry containing all the variables declared globaly in the last frame that ran
+     *                          {[State]} states  An array of the state of the program after each line ran
+     *                          {Boolean} error   wether an error occured during the run
+     *                          {State}   errorState the state containing the error also caontins information about the error see Debugger's doc for properties
+     */
     function runCode() {
         if (!did_set_input) {
             setInput("");
@@ -161,7 +220,7 @@
     /**
      * Run test code that tests the set code
      * should not be used inside the test code
-     * @return {Array} report of test run
+     * @return {Object} report of test run
      */
     function runTest(options) {
         restTest();
@@ -201,30 +260,25 @@
                 throw exc;
             }
         }
-
-        var tests = report.tests;
-        report.passes = _.filter(tests, 'passed');
-        report.failures = _.reject(tests, 'passed');
-        report.score = _.reduce(tests, function(sum, t) {
-            return sum + t.score;
-        }, 0);
-        report.score = Math.max(0, Math.min(report.score, options.exp));
-        report.passed = report.passes.length === tests.length && report.passes.length>0;
-
+        var report = getReport(options);
+        if(options.exp!==undefined) {
+            report.score = Math.max(0, Math.min(report.score, options.exp));
+        }
         return report;
     }
 
     /**
-     * Fire when an error occurrs while parsing or during runtime
+     * Fire testError hook when an error occurrs while parsing or during runtime
      */
     function errorWhileTesting(err) {
         var info = "";
         try {
             info = _b_.getattr(err, 'info');
         } catch (er) {
-            // guess it doesn't work here
+            // guess it doesn't work here, sometimes info doesn't exist when you have a syntax error
         }
         var trace = {
+            event: 'line',
             type: 'runtime_error',
             data: info + '\n' + _b_.getattr(err, '__name__') + ": " + err.$message + '\n',
             stack: err.$stack,
@@ -232,15 +286,29 @@
             name: _b_.getattr(err, '__name__'),
             frame: $B.last(err.$stack),
             err: err,
-            line_no: +($B.last(err.$stack)[1].$line_info.split(',')[0]),
-            module_name: +($B.last(err.$stack)[1].$line_info.split(',')[1])
         };
+        trace.line_no = trace.next_line_no = $B.last(err.$stack)[1].$line_info?(+($B.last(err.$stack)[1].$line_info.split(',')[0])):-1;
+        trace.column_no_start = 0;
+        trace.column_no_stop = 200;
+        if (err.args[1] && err.args[1][1] === trace.line_no) {
+            trace.fragment = err.args[1][3];
+            trace.column_no_start = Math.max(0, err.args[1][2] - 3);
+            trace.column_no_stop = err.args[1][2] + 3;
+        }
         if (trace.name === "SyntaxError") {
             trace.type = 'syntax_error';
         }
         callbacks['testError'](trace, Test);
     }
 
+    /**
+     * appends a single test to the report's test array
+     * this is used by all the tests methods
+     * @param  {Object} test  The test to append in an incomplete form
+     *                        This function will set defaults to the message field and score
+     *                        This function will not allow negative score for passed tests nor positive score for failed tests
+     * @return {Boolean}      wether the test passed or not
+     */
     function appendTestToReport(test) {
         test.message = test.message || DEFAULT_PASS_MESSAGE;
         test.fail_message = test.fail_message || DEFAULT_FAIL_MESSAGE;
@@ -256,18 +324,43 @@
         return test.passed;
     }
 
+    /**
+     * add a passing test to the report with message, score and tag
+     * @return {Boolean} always true
+     */
     function pass() {
         var obj = processPassFailArguments.apply(this, arguments);
         obj.passed = true;
         return appendTestToReport(obj);
     }
 
+    /**
+     * add a failing test to the report with message, score and tag
+     * @return {Boolean} always false
+     */
     function fail() {
         var obj = processPassFailArguments.apply(this, arguments);
         obj.passed = false;
         return appendTestToReport(obj);
     }
 
+    /**
+     * add a test to the report with message, score and tag
+     * the tests passes based on the boolean expression passed as the first argument
+     * @return {Boolean} wether expression was true or not
+     */
+    function assert() {
+        var obj = processAssertArguments.apply(this, arguments);
+        obj.passed = obj.test;
+        return appendTestToReport(obj);
+    }
+
+    /**
+     * Add a test to the report with message, failure message, score and tag
+     * used for strings
+     * The tests passes based on wether the test argument matches to the expected argument which is a regular expression
+     * @return {Boolean} wether the test passed or not
+     */
     function matches() {
         var obj = processArguments.apply(this, arguments);
         var re = new RegExp(obj.expect);
@@ -277,6 +370,11 @@
         return appendTestToReport(obj);
     }
 
+    /**
+     * Add a test to the report with message, failure message, score and tag
+     * The tests passes based on wether the test argument contains the expected argument
+     * @return {Boolean} wether the test passed or not
+     */
     function contains() {
         var obj = processArguments.apply(this, arguments);
         obj.passed = _.includes(obj.test, obj.expect);
@@ -285,6 +383,12 @@
         return appendTestToReport(obj);
     }
 
+
+    /**
+     * Add a test to the report with message, failure message, score and tag
+     * The tests passes based on wether the test argument is deeply equal to the expected argument
+     * @return {Boolean} wether the test passed or not
+     */
     function expect() {
         var obj = processArguments.apply(this, arguments);
         obj.passed = _.isEqual(obj.test,obj.expect);
@@ -293,6 +397,13 @@
         return appendTestToReport(obj);
     }
 
+
+    /**
+     * Add a test to the report with message, failure message, score and tag
+     * The difference from expect is that teh first argument is prepended to the default success and falure message
+     * The tests passes based on wether the test argument is deeply equal to the expected argument
+     * @return {Boolean} wether the test passed or not
+     */
     function expectFor() {
         var premessage = [].shift.call(arguments) || 'inputs';
         var obj = processArguments.apply(this, arguments);
@@ -302,25 +413,32 @@
         return appendTestToReport(obj);
     }
 
+
+    /**
+     * Add a test to the report with message, failure message, score and tag
+     * The tests passes based on wether the test has the expected argument defined
+     * @return {Boolean} wether the test passed or not
+     */
     function exists() {
         var obj = processArguments.apply(this, arguments);
-        obj.passed = (true && obj.test[obj.expect]!==undefined);
+        obj.passed = obj.test[obj.expect]!==undefined;
         obj.message = obj.message || obj.expect+" is defined";
         obj.fail_message = obj.fail_message || obj.expect+" does not exist";
         return appendTestToReport(obj);
     }
 
      /**
-     * Process argumetns that come into test and return them as object
+     * Process arguments that come into test and return them as object
      * @param (String) message  message to write on success or fail
      * @param (Number) score    score awarded for test
      * @param (String) tag      tag assissiated with test
      */
     function processPassFailArguments () {
-        if (_.isObject(arguments[0])) {
-            return arguments[0];
+        var obj = $B.pyobj2jsobj(arguments[0]);
+        if (_.isObject(obj)) {
+            return obj;
         }
-        var obj = {};
+        obj = {};
         switch(arguments.length){
         case 1:
             if(_.isNumber(arguments[0])) {
@@ -346,8 +464,48 @@
         return obj;
     }
 
-    /**
+     /**
      * Process argumetns that come into test and return them as object
+     * @param (Boolean) test    A boolean expression to pass or fail
+     * @param (String) message  message to write on success or fail
+     * @param (Number) score    score awarded for test
+     * @param (String) tag      tag assissiated with test
+     */
+    function processAssertArguments () {
+        var obj = $B.pyobj2jsobj(arguments[0]);
+        if (_.isObject(obj)) {
+            return obj;
+        }
+        obj = {
+            test: arguments[0]
+        };
+        switch(arguments.length){
+        case 2:
+            if(_.isNumber(arguments[1])) {
+                obj.score = arguments[1];
+            } else {
+                obj.message = obj.fail_message = arguments[1];
+            }
+            break;
+        case 3:
+            if(_.isNumber(arguments[1])) {
+                obj.score = arguments[1];
+                obj.tag = arguments[2];
+            } else {
+                obj.message = obj.fail_message = arguments[1];
+                obj.score = arguments[2];
+            }
+            break;
+        default:
+            obj.message = obj.fail_message = arguments[1];
+            obj.score = arguments[2];
+            obj.tag = arguments[3];
+        }
+        return obj;
+    }
+
+    /**
+     * Process arguments that are passed into the test/expect functions and return them as object with al properties set
      * @param test          String test value
      * @param expect      String excpected value of test value
      * @param message       String message to write on success
@@ -408,6 +566,12 @@
         };
     }
 
+    /**
+     * when matching strings in the strign ends in a new line is is removed
+     * This is for convenience as print statements often end with a new line and are used to evaluating the algorithem's output
+     * @param  {Mixed} test could be anythin
+     * @return {Mixed}      if it is a string and has a new line at the end it is removed.
+     */
     function cutLastNewLine(test) {
         if(_.isString(test) && /\n$/.test(test)) {
             return test.substr(0, test.length-1);
@@ -415,15 +579,62 @@
         return test;
     }
 
+    /**
+     * utility functions used to in tests suite when we want to test without appending a test
+     * this is usefule when testing for optional test cases
+     */
     Test.util = $B.jsobj2pyobj({
+            /**
+             * test a regular expression expect with a string test 
+             * @param  {String} expect a regex string
+             * @param  {String} test   string to test
+             * @param  {String} flags  flags for regex
+             * @return {Boolean}        wether the regex matches or not
+             */
             matches: function matches (expect, test, flags) {
                 var re = new RegExp(expect, flags);
                 return re.test(test);
             },
+            /**
+             * run the Javascript string match function on a string test and return the resulting arrays 
+             * Usefule for counting the number of occurance of a pattern using the g flag
+             * and checking if something exists in the code in first place
+             * @param  {String} test   string to match
+             * @param  {String} expect a regex as string
+             * @param  {String} flags  flags for regex
+             * @return {Boolean}       wether the regex matches or not
+             */
             match: function match (test, expect, flags) {
                 var re = new RegExp(expect, flags);
                 return test.match(re);
             },
+            /**
+             * check if a propertu exists in an object
+             * @param  {object} test   an object
+             * @param  {String} expect key to check for
+             * @return {Boolean}        wether the property is defined or not
+             */
+            exists: function exists (test, expect) {
+                return test[expect]!==undefined;
+            },
+            /**
+             * uses lodash's include function on test with prameter expect
+             */
+            includes: function includes (test, expect) {
+                return _.includes(test, expect);
+            },
+            /**
+             * check if test is equal to exp
+             * test if a string will be striped from it's last newline character
+             * @param  {object} test   an object
+             * @param  {String} exp    another object
+             * @return {Boolean}       wether the property is defined or not
+             */
+            expect: function expect (test, exp) {
+                test = cutLastNewLine($B.pyobj2jsobj(test));
+                exp = $B.pyobj2jsobj(exp);
+                return _.isEqual(cutLastNewLine(test),  exp);
+            }
     });
 
     defineModule("Test", Test);
