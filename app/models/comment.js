d262da67ab66c34ec603e19a9c8195df1a24bb11
Abkreno <mohamedelhamamsy789@gmail.com>
2016-04-12 20:04:17 +0200

    Adds check for post/comment/reply owner

--- app/models/comment.js
+++ app/models/comment.js
@@ -4,7 +4,6 @@ import Ember from 'ember';
 export default DS.Model.extend({
   text: DS.attr('string'),
   post: DS.belongsTo('post',{async: true}),
-  question: DS.belongsTo('question',{async: true}),
   author: DS.belongsTo('user',{async: true}),
   totalVotes: DS.attr('number',{defaultValue: 0}),
   replies: DS.hasMany('reply', {async: true}),
